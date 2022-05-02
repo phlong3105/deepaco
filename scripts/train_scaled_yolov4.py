45e5077cafeb128e6bacba876c0e59f4f86fd368
longpham <longpham3105@gmail.com>
2022-05-03 03:26:19 +0900

    Fix issues

--- scripts/train_scaled_yolov4.py
+++ scripts/train_scaled_yolov4.py
@@ -53,9 +53,9 @@ train_configs = {
     },
     "yolov4-p5_aic22retail117_896" : {
         "weights"        : os.path.join(pretrained_dir, "scaled_yolov4", "yolov4-p5_coco.pt"),
-        "cfg"            : os.path.join(yolov4_root, "models", "yolov4-p5.yaml"),
+        "cfg"            : "yolov4-p5.yaml",
         "data"           : "aic22retail117.yaml",
-        "hyp"            : os.path.join(yolov4_root, "data",   "hyp.scratch.yaml"),
+        "hyp"            : "hyp.scratch.yaml",
         "epochs"         : 50,
         "batch_size"     : 4,
         "img_size"       : [896, 896],
@@ -78,9 +78,9 @@ train_configs = {
     },
     "yolov4-p7_aic22retail117_1536": {
         "weights"        : os.path.join(pretrained_dir, "scaled_yolov4", "yolov4-p7_coco.pt"),
-        "cfg"            : os.path.join(yolov4_root, "models", "yolov4-p7.yaml"),
+        "cfg"            : "yolov4-p7.yaml",
         "data"           : "aic22retail117.yaml",
-        "hyp"            : os.path.join(yolov4_root, "data",   "hyp.scratch.yaml"),
+        "hyp"            : "hyp.scratch.yaml",
         "epochs"         : 20,
         "batch_size"     : 4,
         "img_size"       : [1536, 1536],
