5eef09cb5d9e9350fde37c0df2c4705eca2a4a73
phlong3105 <longpham3105@gmail.com>
2022-05-08 14:42:37 +0900

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -32,8 +32,8 @@ case "$OSTYPE" in
     pip install --upgrade pip
 
     # Install `mish-cuda`
-    echo "Install 'mish-cuda':"
-    pip install git+https://github.com/JunnYu/mish-cuda.git
+    # echo "Install 'mish-cuda':"
+    # pip install git+https://github.com/JunnYu/mish-cuda.git
 
     # Remove `cv2/plugin` folder:
     rm -rf $CONDA_PREFIX/lib/python3.9/site-packes/cv2/plugin
